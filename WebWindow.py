90a09e60086725491e26407d5b7e8091933e8c21
Dylan Halls <Dylan.halls123@gmail.com>
2017-02-06 16:20:28 +0000

    Add files via upload

--- WebWindow.py
+++ WebWindow.py
@@ -1,10 +1,15 @@
 # -*- coding: utf-8 -*-
 
-import gtk, socket, gobject
+import gtk, gobject
 
 hosts = []
 Accept_Language = []
 Accept_Encoding = []
+Source_Address = []
+typ = []
+Dst_Address = []
+Source_Port = []
+Dst_Port = []
 User_Agent = []
 Accept = []
 Referer = []
@@ -16,11 +21,45 @@ Cache_Control = []
 Cookie = []
 DNT = []
 
+Status_Line = []
+Age = []
+E_Tag = []
+Location = []
+Proxy_Authenticate = []
+Retry_After = []
+Server = []
+Vary = []
+WWW_Authenticate = []
+Cache_Control = []
+Connection = []
+Date = []
+Pragma = []
+Trailer = []
+Transfer_Encoding = []
+Upgrade = []
+Via = []
+Warning = []
+Keep_Alive = []
+Allow = []
+Content_Encoding = []
+Content_Language = []
+Content_Length = []
+Content_Location = []
+Content_MD5 = []
+Content_Range = []
+Content_Type = []
+Expires = []
+Last_Modified = []
+Headers = []
+Additional_Headers = []
 
-with open('Stalker.log', 'rb') as file:
+with open('Stalker.log', 'r') as file:
     raw_data = file.readlines()
     file.close()
     for i in raw_data:
+        
+        #Request list makers
+        
         if 'Host:' in i:
             host = i[5:].strip().replace(" ", '')
             hosts.append(host)
@@ -48,10 +87,81 @@ with open('Stalker.log', 'rb') as file:
             Cache_Control.append(i[14:].strip())
         if 'Cookie:' in i:
             Cookie.append(i[7:].strip())
-
-
-print(hosts)
-
+        if 'Source-Address:' in i:
+            Source_Address.append(i[16:].strip())
+        if 'Destination-Address:' in i:
+            Dst_Address.append(i[21:].strip())
+        if 'Destination_Port:' in i:
+            Dst_Port.append(i[17:].strip())
+        if 'Source_Port:' in i:
+            Source_Port.append(i[13:].strip())
+        if 'HTTP-Type:' in i:
+            typ.append(i[11:])
+        
+        #Responce list makers
+        
+        if "HTTP/1.1 " in i:
+            Status_Line.append(i)
+        if "Age:" in  i:
+            Age.append(i[5:])
+        if "ETag" in i:
+            E_Tag.append(i[7:])
+        if "Location" in i:
+            Location.append(i[10:])
+        if "Proxy-Authenticate" in i:
+            Proxy_Authenticate.append(i[20:])
+        if "Retry-After" in i:
+            Retry_After.append(i[13:])
+        if "Server" in i:
+            Server.append(i[8:])
+        if "Vary" in i:
+            Vary.append(i[6:])
+        if "WWW-Authenticate" in i:
+            WWW_Authenticate.append(i[18:])
+        if "Cache-Control" in i:
+            Cache_Control.append(i[15:])
+        if "Connection" in i:
+            Connection.append(i[12:])
+        if "Date: " in i:
+            Date.append(i[6:])
+        if "Pragma" in i:
+            Pragma.append(i[8:])
+        if "Trailer" in i:
+            Trailer.append(i[9:])
+        if "Transfer-Encoding" in i:
+            Transfer_Encoding.append(i[19:])
+        if "Upgrade" in i:
+            Upgrade.append(i[9:])
+        if "Via" in i:
+            Via.append(i[5:])
+        if "Warning" in i:
+            Warning.append(i[9:])
+        if "Keep-Alive" in i:
+            Keep_Alive.append(i[12:])
+        if "Allow" in i:
+            Allow.append(i[7:])
+        if "Content-Encoding" in i:
+            Content_Encoding.append(i[18:])
+        if "Content-Language" in i:
+            Content_Language.append(i[18:])
+        if "Content-Length" in i:
+            Content_Length.append(i[16:])
+        if "Content-Location" in i:
+            Content_Location.append(i[18:])
+        if "Content-MD5" in i:
+            Content_MD5.append(i[11:])
+        if "Content-Range" in i:
+            Content_Range.append(i[15:])
+        if "Content-Type" in i:
+            Content_Type.append(i[14:])
+        if "Expires" in i:
+            Expires.append(i[9:])
+        if "Last-Modified" in i:
+            Last_Modified.append(i[15:])
+        if "Headers" in i:
+            Headers.append(i[9:])
+        if "Additional-Headers" in i:
+            Additional_Headers.append(i[20:])
 
 class PyApp(gtk.Window):
    
@@ -63,29 +173,53 @@ class PyApp(gtk.Window):
       vbox = gtk.VBox(False, 5)
       scw = gtk.ScrolledWindow(None, None)
       
-      data_store = gtk.TreeStore(str, str)
+      data_store = gtk.TreeStore(str, str, str)
 
       len_hosts = len(hosts)
       for i in range(len_hosts):
           try:
               try:
-                  row = data_store.append(None,[Method[i], hosts[i]])
-                  data_store.append(row, ['HTTP Version:', HTTP_Version[i]])
-                  data_store.append(row, ['Request:', Get_Method[i]])
-                  data_store.append(row, ['User-Agent:', User_Agent[i]])
-                  data_store.append(row, ['Referer:', Referer[i]])
-                  data_store.append(row, ['Cookie:', Cookie[i]])
-                  data_store.append(row, ['Accept-Language:', Accept_Language[i]])
-                  data_store.append(row, ['Accept-Encoding:', Accept_Encoding[i]])
-                  data_store.append(row, ['Connection', Connection[i]])
-                  data_store.append(row, ['Accept:', Accept[i]])
-                  data_store.append(row, ['DNT:', DNT[i]])
+                  if typ[i] == 'HTTP Request\n':
+                      row = data_store.append(None,['HTTP Request', Method[i], hosts[i]])
+                      data_store.append(row, ['', 'Source Mac:', Source_Address[i]])
+                      data_store.append(row, ['', 'Destination Mac:', Dst_Address[i]])
+                      data_store.append(row, ['', 'Source Port:', Source_Port[i]])
+                      data_store.append(row, ['', 'Destination Port:', Dst_Port[i]])
+                      data_store.append(row, ['', '         HTTP Version:', HTTP_Version[i]])
+                      data_store.append(row, ['', '         Request:', Get_Method[i]])
+                      data_store.append(row, ['', '         User-Agent:', User_Agent[i]])
+                      data_store.append(row, ['', '         Referer:', Referer[i]])
+                      data_store.append(row, ['', '         Cookie:', Cookie[i]])
+                      data_store.append(row, ['', '         Accept-Language:', Accept_Language[i]])
+                      data_store.append(row, ['', '         Accept-Encoding:', Accept_Encoding[i]])
+                      data_store.append(row, ['', '         Connection', Connection[i]])
+                      data_store.append(row, ['', '         Accept:', Accept[i]])
+                      data_store.append(row, ['', '         DNT:', DNT[i]])
               except IndexError: pass
           except UnboundLocalError: pass
 
+      len_r = len(Status_Line)
+      for o in range(len_r):
+        try:
+            if typ[o] == 'HTTP Responce\n':
+                print('HTTP Request')
+                row = data_store.append(None,['HTTP Responce', '', 'http://www.bbc.com/'])
+                data_store.append(row, ['', 'Source Mac:', Source_Address[o]])
+                data_store.append(row, ['', 'Destination Mac:', Dst_Address[o]])
+                data_store.append(row, ['', 'Source Port:', Source_Port[o]])
+                data_store.append(row, ['', 'Destination Port:', Dst_Port[o]])
+                data_store.append(row, ['', '         Status-Line:', Status_Line[o].strip()])
+                data_store.append(row, ['', '         Server:', Server[o].strip()])
+                data_store.append(row, ['', '         Via:', Via[o].strip()])
+                data_store.append(row, ['', '         Location:', Location[o].strip()])
+                data_store.append(row, ['', '         Content-Length:', Content_Length[o].strip()])
+
+        except IndexError: pass
+      
+
       data_tree_view = gtk.TreeView(data_store)
 
-      for i, colt in enumerate(["Method", "Address"]):
+      for i, colt in enumerate(["Type", "Method", "Address"]):
           renderer = gtk.CellRendererText()
           column = gtk.TreeViewColumn(colt, renderer, text=i)
           fro = data_tree_view.append_column(column)
--- urlsnuffler.py
+++ urlsnuffler.py
@@ -21,6 +21,11 @@ class Sniffer(object):
             x = -1
             urls = []
             for i in http:
+                file.write("HTTP-Type: HTTP Request\n")
+                file.write("Source-Address: {}\n".format(pkt.src))
+                file.write("Destination-Address: {}\n".format(pkt.dst))
+                file.write("Destination_Port: {}\n".format(pkt.dport))
+                file.write("Source_Port: {}\n".format(pkt.sport))
                 file.write("{}\n".format(http[x]))
                 x += 1
                 try:
@@ -49,6 +54,120 @@ class Sniffer(object):
                         c = http[x].replace("Cookie:", '')
                         print("\""+h+g[4:]+"\"","-","\"{0}\" -{1} - {2} -\033[1;33m{3}\033[00m".format(oh, u, r, c))
                     except UnboundLocalError: pass
+
+        elif pkt.haslayer(scapy_http.http.HTTPResponse):
+            http = pkt[scapy_http.http.HTTPResponse]
+            file =  open('Stalker.log', 'ab')
+            http = str(http).splitlines()
+            x = -1
+            urls = []
+            for i in http:
+                file.write("HTTP-Type: HTTP Responce\n")
+                file.write("Source-Address: {}\n".format(pkt.src))
+                file.write("Destination-Address: {}\n".format(pkt.dst))
+                file.write("Destination_Port: {}\n".format(pkt.dport))
+                file.write("Source_Port: {}\n".format(pkt.sport))
+                x += 1
+                if "HTTP/1.1 " in http[x]:
+                    sl = http[x]
+                    file.write(http[x])
+                if "Age:" in  http[x]:
+                    age = http[x]
+                    file.write(http[x]+'\n')
+                if "ETag" in http[x]:
+                    et = http[x]
+                    file.write(http[x]+'\n')
+                if "Location" in http[x]:
+                    loc = http[x]
+                    file.write(http[x]+'\n')
+                if "Proxy-Authenticate" in http[x]:
+                    pa = http[x]
+                    file.write(http[x]+'\n')
+                if "Retry-After" in http[x]:
+                    ra = http[x]
+                    file.write(http[x]+'\n')
+                if "Server" in http[x]:
+                    s = http[x]
+                    file.write(http[x]+'\n')
+                if "Vary" in http[x]:
+                    v = http[x]
+                    file.write(http[x]+'\n')
+                if "WWW-Authenticate" in http[x]:
+                    wwa = http[x]
+                    file.write(http[x]+'\n')
+                if "Cache-Control" in http[x]:
+                    cc = http[x]
+                    file.write(http[x]+'\n')
+                if "Connection" in http[x]:
+                    conn = http[x]
+                    file.write(http[x]+'\n')
+                if "Date: " in http[x]:
+                    dat = http[x]
+                    file.write(http[x]+'\n')
+                if "Pragma" in http[x]:
+                    pra = http[x]
+                    file.write(http[x]+'\n')
+                if "Trailer" in http[x]:
+                    tra = http[x]
+                    file.write(http[x]+'\n')
+                if "Transfer-Encoding" in http[x]:
+                    te = http[x]
+                    file.write(http[x]+'\n')
+                if "Upgrade" in http[x]:
+                    upg = http[x]
+                    file.write(http[x]+'\n')
+                if "Via" in http[x]:
+                    via = http[x]
+                    file.write(http[x]+'\n')
+                if "Warning" in http[x]:
+                    warn = http[x]
+                    file.write(http[x]+'\n')
+                if "Keep-Alive" in http[x]:
+                    ka = http[x]
+                    file.write(http[x]+'\n')
+                if "Allow" in http[x]:
+                    al = http[x]
+                    file.write(http[x]+'\n')
+                if "Content-Encoding" in http[x]:
+                    coe = http[x]
+                    file.write(http[x]+'\n')
+                if "Content-Language" in http[x]:
+                    col = http[x]
+                    file.write(http[x]+'\n')
+                if "Content-Length" in http[x]:
+                    cole = http[x]
+                    file.write(http[x]+'\n')
+                if "Content-Location" in http[x]:
+                    colo = http[x]
+                    file.write(http[x]+'\n')
+                if "Content-MD5" in http[x]:
+                    comd = http[x]
+                    file.write(http[x]+'\n')
+                if "Content-Range" in http[x]:
+                    cora = http[x]
+                    file.write(http[x]+'\n')
+                if "Content-Type" in http[x]:
+                    coty = http[x]
+                    file.write(http[x]+'\n')
+                if "Expires" in http[x]:
+                    ex = http[x]
+                    file.write(http[x]+'\n')
+                if "Last-Modified" in http[x]:
+                    lamo = http[x]
+                    file.write(http[x]+'\n')
+                if "Headers" in http[x]:
+                    hea = http[x]
+                    file.write(http[x]+'\n')
+                if "Additional-Headers" in http[x]:
+                    adhe = http[x]
+                    file.write(http[x]+'\n')
+
+                file.write('\n')
+                try: 
+                    #31
+                    #26
+                    print("{} - {} - {} - {} - {} - {} - {} - {} - {} - {} - {} - {} - {} - {} - {} - {} - {} - {} - {} - {} - {} - {} - {} - {} - {} - {} - {} - {} - {} - {} - {}".format(sl, age, et, loc, pa, ra, s, v, wwwa, cc, conn, dat, pra, tra, te, upg, via, warn, ka, al, coe, col, cole, colo, comd, cora, coty, ex, lamo, hea, adhe))               
+                except UnboundLocalError: pass
 
 def main():
     sn = Sniffer()
